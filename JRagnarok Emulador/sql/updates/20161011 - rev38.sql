45356d0d12bb43e44897978a30e5e467ba0bed56
Andrew Mello <driwundernight@gmail.com>
2016-10-11 16:45:46 -0300

    1.0.38 - Enviando arquivo para atualizar o banco de dados SQL com as novas tabelas. 	- Também está sendo incluso a alteração de colunas de login_log.

--- JRagnarok Emulador/sql/updates/20161011 - rev38.sql
+++ JRagnarok Emulador/sql/updates/20161011 - rev38.sql
@@ -4,11 +4,11 @@ CREATE TABLE IF NOT EXISTS logins
 	id INT AUTO_INCREMENT,
 	username VARCHAR(23) NOT NULL,
 	password VARCHAR(32) NOT NULL,
-	last_login DATETIME NOT NULL DEFAULT CURRENT TIMESTAMP,
+	last_login DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	registered DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
 	UNIQUE (username),
-	PRIMARY KEY (id),
+	PRIMARY KEY (id)
 
 ) ENGINE=MyISAM;
 
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS pincodes
 	code CHAR(4) NOT NULL DEFAULT '0000',
 	changed DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00',
 
-	PRIMARY KEY (id),
+	PRIMARY KEY (id)
 
 ) ENGINE=MyISAM;
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS groups
 	log_enabled TINYINT(1) NOT NULL DEFAULT 0,
 
 	PRIMARY KEY (id),
-	FOREIGN KEY (parent) REFERENCES (groups),
+	CONSTRAINT SelfKey FOREIGN KEY (parent) REFERENCES groups (id) ON DELETE NO ACTION ON UPDATE NO ACTION
 
 ) ENGINE=MyISAM;
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS commands
 	id INT AUTO_INCREMENT,
 	name VARCHAR(24) NOT NULL,
 
-	PRIMARY KEY (id),
+	PRIMARY KEY (id)
 
 ) ENGINE=MyISAM;
 
@@ -50,31 +50,31 @@ CREATE TABLE IF NOT EXISTS permissions
 	id INT AUTO_INCREMENT,
 	name VARCHAR(24) NOT NULL,
 
-	PRIMARY KEY (id),
+	PRIMARY KEY (id)
 
 ) ENGINE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS groups_commands
 (
-	group INT NOT NULL,
+	groupid INT NOT NULL,
 	command INT NOT NULL,
 	enabled TINYINT(1) NOT NULL DEFAULT 0,
 
 	PRIMARY KEY (group, command),
 	FOREIGN KEY (group) REFERENCES groups (id),
-	FOREIGN KEY (command) REFERENCES commands (id),
+	FOREIGN KEY (command) REFERENCES commands (id)
 
 ) ENGINE=MyISAM;
 
 CREATE TABLE IF NOT EXISTS groups_permissions
 (
-	group INT NOT NULL,
+	groupid INT NOT NULL,
 	permission INT NOT NULL,
 	enabled TINYINT(1) NOT NULL DEFAULT 0,
 
-	PRIMARY KEY (group, permission),
-	FOREIGN KEY (group) REFERENCES groups (id),
-	FOREIGN KEY (permission) REFERENCES permissions (id),
+	PRIMARY KEY (groupid, permission),
+	FOREIGN KEY (groupid) REFERENCES groups (id),
+	FOREIGN KEY (permission) REFERENCES permissions (id)
 
 ) ENGINE=MyISAM;
 
@@ -82,12 +82,12 @@ CREATE TABLE IF NOT EXISTS vip
 (
 	id INT AUTO_INCREMENT,
 	name VARCHAR(24) NOT NULL,
-	group INT NOT NULL,
+	groupid INT NOT NULL,
 	char_slot_count TINYINT NOT NULL,
 	max_storage SMALLINT NOT NULL,
 
-	PRIMARY KEY (id,
-	FOREIGN KEY (group) REFERENCES groups (id),
+	PRIMARY KEY (id),
+	FOREIGN KEY (groupid) REFERENCES groups (id)
 
 ) ENGINE=MyISAM;
 
@@ -102,13 +102,13 @@ CREATE TABLE IF NOT EXISTS accounts
 	expiration DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00',
 	pincode INT NOT NULL,
 	state TINYINT NOT NULL DEFAULT 0,
-	group INT NOT NULL,
+	groupid INT NOT NULL,
 	last_ip VARCHAR(15) NOT NULL DEFAULT '127.0.0.1',
 
 	UNIQUE (email),
 	PRIMARY KEY (login),
 	FOREIGN KEY (pincode) REFERENCES pincodes (id),
-	FOREIGN KEY (group) REFERENCES groups (id),
+	FOREIGN KEY (groupid) REFERENCES groups (id)
 
 ) ENGINE=MyISAM;
 
@@ -120,30 +120,21 @@ CREATE TABLE IF NOT EXISTS accounts_vip
 	vip INT,
 	timeout DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00',
 
-	PRIMARY KEY (account),
+	PRIMARY KEY (account)
 
 ) ENGINE=MyISAM;
 
-CREATE TABLE IF NOT EXISTS login_log
-(
-	time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	ip INT NOT NULL DEFAULT '127.0.0.1',
-	login INT NOT NULL,
-	rcode TINYINT NOT NULL,
-	message VARCHAR(255)
-
-	FOREIGN KEY (login) REFERENCES logins (id),
-
-) ENGINE=MyISAM;
+ALTER TABLE login_log CHANGE ip ip INT NOT NULL DEFAULT 2130706433;
+ALTER TABLE login_log CHANGE username login INT NOT NULL;
+ALTER TABLE login_log ADD FOREIGN KEY (login) REFERENCES logins (id);
 
 CREATE TABLE login_log
 (
 	time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	ip INT NOT NULL DEFAULT 2130706433,
-	login INT NOT NULL,
+	ip VARCHAR(15) NOT NULL DEFAULT '127.0.0.1',
+	username VARCHAR(24) NOT NULL,
 	rcode TINYINT NOT NULL,
-	message VARCHAR(255),
-
-	FOREIGN KEY (login) REFERENCES logins (id)
-
+	message VARCHAR(255)
 ) ENGINE=MyISAM;
+
+INSERT INTO logins (id, username, password) VALUES (0, 'server', 'jragnarok');
